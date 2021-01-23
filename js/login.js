a3da19cef4633a11c49541adb2720cf4740c6b2e
João Tomás <jtsimoes@ua.pt>
2021-01-23 01:49:03 +0000

    Fixed minor bug on login checker script

--- js/login.js
+++ js/login.js
@@ -2,7 +2,7 @@
 if(typeof(Storage) !== "undefined"){
     if (localStorage.getItem('ecomarket_session')){
         // Let the user pass and get session data from the storage item
-        var ecomarket_user = localStorage.getItem('ecomarket_session');
+        var ecomarket_user = JSON.parse(localStorage.getItem('ecomarket_session'));
     } else{
         // Send the user to the login page
         window.location.replace("/ams_g202/marketplace/login");
